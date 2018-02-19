ffc3452c738e8ab848361b1c602fbced8b554c74
Guillermo Martín <xguillemartinx@gmail.com>
2018-02-19 17:56:13 -0300

    Implementation of suggestions.

---
+++ app/src/main/java/io/gmartin/deofertas/controllers/SuggestionController.java
@@ -0,0 +1,101 @@
+package io.gmartin.deofertas.controllers;
+
+import android.content.Context;
+
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.gmartin.deofertas.models.SuggestedOffer;
+
+
+public class SuggestionController extends BaseController {
+
+    private SuggestionControllerListener mSuggestionControllerListener;
+
+    public interface SuggestionControllerListener {
+        void onLastSuggestionDataReceived(SuggestedOffer suggestedOffer);
+    }
+
+    public SuggestionController(Context context){
+        super(context);
+
+        if (mContext instanceof BaseControllerListener) {
+            mListener = (BaseControllerListener) mContext;
+        }
+
+        if (mContext instanceof SuggestionControllerListener) {
+            mSuggestionControllerListener = (SuggestionControllerListener) mContext;
+        }
+    }
+
+    public void fetchSuggestions(){
+        String endPoint = "/suggestion";
+
+        try {
+            RestClient.get(mURL + endPoint, new RestClient.Result() {
+
+                @Override
+                public void onResult(Object result) {
+                    List<SuggestedOffer> suggestionsList = new ArrayList<>();
+                    JSONArray jsonArray = (JSONArray) result;
+                    JSONObject jsonObject;
+                    Gson gson = new Gson();
+
+                    for(int i=0; i < jsonArray.length(); i++) {
+                        try {
+                            jsonObject = jsonArray.getJSONObject(i);
+                            suggestionsList.add(gson.fromJson(jsonObject.toString(), SuggestedOffer.class));
+                        } catch (Exception e) {
+
+                        }
+                    }
+
+                    mListener.onDataReceived(suggestionsList);
+                }
+
+                @Override
+                public void onError(String message) {
+                    mListener.onErrorEvent(message);
+                }
+            });
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void fetchLastSuggestion(){
+        String endPoint = "/suggestion/last";
+
+        try {
+            RestClient.get(mURL + endPoint, new RestClient.Result() {
+
+                @Override
+                public void onResult(Object result) {
+                    SuggestedOffer suggestion = null;
+                    JSONObject jsonObject = (JSONObject) result;
+                    Gson gson = new Gson();
+
+                    try {
+                        suggestion = gson.fromJson(jsonObject.toString(), SuggestedOffer.class);
+                    } catch (Exception e) {
+
+                    }
+
+                    mSuggestionControllerListener.onLastSuggestionDataReceived(suggestion);
+                }
+
+                @Override
+                public void onError(String message) {
+                    mListener.onErrorEvent(message);
+                }
+            });
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
